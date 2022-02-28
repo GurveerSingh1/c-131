ecddf991da5e945a88cc75dde525f1b757ffc2f1
GurveerSingh1 <81967740+GurveerSingh1@users.noreply.github.com>
2022-02-28 18:28:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+img=""
+status = "";
+object=[];
+
+
+
+function setup(){
+
+    canvas=createCanvas(640,420);
+    canvas.center()
+    objectDetector=ml5.objectDetector('cocossd', modelLoded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function preload(){
+    img=loadImage("dog_cat.jpg")
+}
+
+function draw(){
+    image(img, 0 ,0, 640, 420);
+   if(status != ""){
+    for(i=0; i < object.length; i++){
+        document.getElementById("status").innerHTML= "Status: Object Detected";
+        fill("#FF000");
+        percent = floor(object[i].confidence*100)
+        text(object[i].label + percent + "%",  object[i].x + 15, object[i].y + 15);
+        noFill();
+        stroke("#FF000");
+        rect(object[i].x, object[i].y, object[i].width, object[i].height);
+    }
+   }
+}
+
+function modelLoded()
+{
+    console.log("Model Loded !")
+    status=true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    object=results;
+
+}
+
